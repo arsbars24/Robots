3ebd9b487046f6293cbcc46edb582151e69d2ac3
arsbars24 <arsik.1104@mail.ru>
2024-05-14 13:22:07 +0500

    Task 5 fix commit

--- src/main/java/gui/LogWindow.java
+++ src/main/java/gui/LogWindow.java
@@ -10,12 +10,12 @@ import javax.swing.JPanel;
 import log.LogChangeListener;
 import log.LogEntry;
 import log.LogWindowSource;
+import locale.LocaleManager;
 
 /**
  * Окно протокола работы приложения.
  */
-public class LogWindow extends JInternalFrame implements LogChangeListener
-{
+public class LogWindow extends JInternalFrame implements LogChangeListener {
     private final LogWindowSource m_logSource;
     private final TextArea m_logContent;
 
@@ -25,15 +25,14 @@ public class LogWindow extends JInternalFrame implements LogChangeListener
      *
      * @param logSource Источник данных для протокола.
      */
-    public LogWindow(LogWindowSource logSource) 
-    {
-        super("Протокол работы", true,
-                true, true, true);
+    public LogWindow(LogWindowSource logSource) {
+        super(LocaleManager.getCurrentResource(LocaleManager.getCurrentLanguage()).getString("log_window_title"),
+                true, true, true, true);
         m_logSource = logSource;
         m_logSource.registerListener(this);
         m_logContent = new TextArea("");
         m_logContent.setSize(200, 500);
-        
+
         JPanel panel = new JPanel(new BorderLayout());
         panel.add(m_logContent, BorderLayout.CENTER);
         getContentPane().add(panel);
@@ -44,11 +43,9 @@ public class LogWindow extends JInternalFrame implements LogChangeListener
     /**
      * Обновляет содержимое протокола.
      */
-    private void updateLogContent()
-    {
+    private void updateLogContent() {
         StringBuilder content = new StringBuilder();
-        for (LogEntry entry : m_logSource.all())
-        {
+        for (LogEntry entry : m_logSource.all()) {
             content.append(entry.getMessage()).append("\n");
         }
         m_logContent.setText(content.toString());
@@ -63,8 +60,7 @@ public class LogWindow extends JInternalFrame implements LogChangeListener
      * работы с графическим интерфейсом Swing.
      */
     @Override
-    public void onLogChanged()
-    {
+    public void onLogChanged() {
         EventQueue.invokeLater(this::updateLogContent);
     }
 }
